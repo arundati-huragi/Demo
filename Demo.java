dea5d6d437f9aec9f097b7f206b27863fa8c4221
arundati-huragi <arundatikhuragi@gmail.com>
2025-04-23 14:55:28 +0530

    it contains addition and substraction

---
+++ Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String[] args) {
+        int num1 = 10; 
+        int num2 = 5;
+        int sum = num1 + num2;
+        int sub = num1 - num2;
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+        System.out.println("The subtraction of " + num1 + " and " + num2 + " is: " +sub);
+    }}
